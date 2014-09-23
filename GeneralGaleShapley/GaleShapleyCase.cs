ce33ef228be44e1ab15747c87c2a380d29116169
htellez <tellez.hector@gmail.com>
2014-09-23 14:06:09 -0500

    Adding preferences states.

--- GeneralGaleShapley/GaleShapleyCase.cs
+++ GeneralGaleShapley/GaleShapleyCase.cs
@@ -1,7 +1,16 @@
 ﻿using System;
+using System.Linq;
 
 namespace GeneralGaleShapley
 {
+	public enum States
+	{
+		Unknown = -1,
+		Unrequested = 0,
+		Matched = 1,
+		Rejected = 2
+	}
+
 	public class GaleShapleyCase
 	{
 		public GaleShapleyCase(int n) : this(n, n){}
@@ -12,6 +21,10 @@ namespace GeneralGaleShapley
 
 		public GaleShapleyCase (int men, int women)
 		{
+			if (men <= 0 || women <= 0)
+			{
+				throw new NotSupportedException ("Only positive integers allowed.");
+			}
 			Men = men;
 			Women = women;
 		}
@@ -20,24 +33,33 @@ namespace GeneralGaleShapley
 
 		int[][] _womenMatrix;
 
+		States[][] _menStatesMatrix;
+
+		States[][] _womenStatesMatrix;
+
 		public int[][] MenMatrix 
 		{
 			get
 			{
-				if(_menMatrix != null)
+				if(_menMatrix == null)
 				{
-					return _menMatrix;
+					_menMatrix = InitializePreferencesMatrix (Men, Women);
 				}
-
-				_menMatrix = new int[Men][];
-			
-				Permutations permutations = new Permutations(Women);
-				for(int i = 0; i < Men; i++)
-				{
-					_menMatrix [i] = permutations.RandomPermutation().ToArray();
-				}
-
+		
 				return _menMatrix; 
+			}
+		}
+
+		public States[][] MenStatesMatrix
+		{
+			get
+			{
+				if (_menStatesMatrix == null)
+				{
+					_menStatesMatrix = InitializeStatesMatrix (Men, Women);
+				}
+
+				return _menStatesMatrix;
 			}
 		}
 
@@ -45,22 +67,80 @@ namespace GeneralGaleShapley
 		{
 			get
 			{
-				if(_womenMatrix != null)
+				if(_womenMatrix == null)
 				{
-					return _womenMatrix;
-				}
-
-				_womenMatrix = new int[Women][];
-
-				Permutations permutations = new Permutations(Men);
-				for(int i = 0; i < Men; i++)
-				{
-					_womenMatrix [i] = permutations.RandomPermutation().ToArray();
+					_womenMatrix = InitializePreferencesMatrix (Women, Men);
 				}
 
 				return _womenMatrix; 
 			}
 		}
-	}
-}
 
+		public States[][] WomenStatesMatrix
+		{
+			get
+			{ 
+				if (_womenStatesMatrix == null)
+				{
+					_womenStatesMatrix = InitializeStatesMatrix (Women, Men);
+				}
+
+				return _womenStatesMatrix;
+			}
+		}
+
+		States[][] InitializeStatesMatrix (int persons, int preferencesSize)
+		{
+			var matrix = new States[persons][];
+			var preflength = matrix [0].Length;
+			for(int i = 0; i < persons; i++)
+			{
+
+				for (int j = 0; j < preflength; i++)
+				{
+					matrix [i] [j] = States.Unrequested;
+				}
+			}
+
+			return matrix; 		
+		}
+
+		private int[][] InitializePreferencesMatrix(int persons, int preferencesSize){
+			var matrix = new int[persons][];
+			Permutations permutations = new Permutations(preferencesSize);
+			for(int i = 0; i < persons; i++)
+			{
+				matrix [i] = permutations.RandomPermutation().ToArray();
+			}
+
+			return matrix; 
+		}
+
+		public void Print()
+		{
+			Console.WriteLine ("Men's matrix:");
+			PrintMatrix (MenMatrix);
+			Console.WriteLine ("Women's matrix:");
+			PrintMatrix (WomenMatrix);
+		}
+
+		private void PrintMatrix (int[][] matrix)
+		{
+			int persons = matrix.Length;
+			int preferencesLength = matrix [0].Length;
+			int pad1 = persons.ToString().Length + 2;
+			int pad2 = preferencesLength.ToString().Length + 2; 
+			int lineLengt = pad1 + 1 + ((pad2 + 1) * preferencesLength);
+	
+			Console.WriteLine(new String('-', lineLengt));
+			for (int i = 0; i < persons; i++)
+			{
+				string lineName = i.ToString().PadRight(pad1);
+				string preferences = string.Join(",", matrix[i].Select(s => s.ToString().PadLeft(pad2)));
+				string line = string.Format("{0}|{1}", lineName, preferences);
+				Console.WriteLine (line);
+			}
+			Console.WriteLine(new String('-', lineLengt));
+		}
+	} 
+}
--- GeneralGaleShapley/Program.cs
+++ GeneralGaleShapley/Program.cs
@@ -6,12 +6,8 @@ namespace GeneralGaleShapley
 	{
 		public static void Main (string[] args)
 		{
-
-			var permutations = new Permutations (3);
-			for (int i = 0; i < 30; i++)
-			{
-				Console.WriteLine ("{0} {1}", i, string.Join(",", permutations.RandomPermutation ()));
-			}
+			var gs = new GaleShapleyCase (5, 3);
+			gs.Print ();
 		}
 	}
 
