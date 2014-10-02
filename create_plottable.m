5a7370f0d24aa21b99225fdfd458d0c3f56eeac7
htellez <tellez.hector@gmail.com>
2014-10-02 15:27:42 -0500

    Improving plotting functions

--- create_plottable.m
+++ create_plottable.m
@@ -1,9 +1,10 @@
-function void = create_plottable()
-    [data, l] = read_data();
-    means = mean(data);
-    i  = 3*(0:(l-1));
-    n = means(i+1);
-    iterations = means(i+2);
-    y = [n; iterations; n; n; n; n .* n; n; n .* log(n); n; 2 * (n .* log(n))];
-    dlmwrite("plottable.txt", y', " ");
-endfunction
+[data, l] = read_data();
+means = mean(data);
+i  = 3*(0:(l-1));
+n = means(i+1);
+iterations = means(i+2);
+variances = var(data, 1)(i+2);
+deviations = sqrt(variances);
+nlogn = n .* log(n);
+y = [n; iterations; deviations; n; n; n; n .* n; n; nlogn; n; 2 .* (nlogn)];
+dlmwrite("plottable.txt", y', " ");
--- plot.sh
+++ plot.sh
@@ -1,4 +1,9 @@
 set terminal postscript eps enhanced color font 'Helvetica,10'
 set output 'results.ps'
 set xlabel 'Case size'
-plot 'plottable.txt' using 1:2 with lines title "Classic Galey-Shapley", '' using 3:4 with lines title "n", '' using 7:8 with lines title "n log(n)", '' using 9:10 with lines title "2n log(n)"
+plot    'plottable.txt' using 1:2:3 with yerrorbars lt 1 lc rgb "#FF0000" title "error", \
+        '' using 1:2 with lines lt rgb "#000000" title "Classic Galey-Shapley", \
+        '' using 4:5 with lines title "n", \
+        '' using 8:9 with lines lt rgb "#0000FF" title "n log(n)", \
+        '' using 10:11 with lines title "2n log(n)"
+
